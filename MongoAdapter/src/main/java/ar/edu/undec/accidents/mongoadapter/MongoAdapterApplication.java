6e900fe302c4b6c1e3e6ed9d7e376060b6223b9f
Javier Ruitti <jruitti@undec.edu.ar>
2020-08-27 12:07:10 -0300

    Agrega modulo Adapter para MongoDB

---
+++ MongoAdapter/src/main/java/ar/edu/undec/accidents/mongoadapter/MongoAdapterApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.accidents.mongoadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoAdapterApplication.class, args);
+    }
+
+}
