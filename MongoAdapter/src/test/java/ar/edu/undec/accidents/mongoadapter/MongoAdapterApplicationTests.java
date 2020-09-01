1422d69c73303975dacaead4695ddcd0e6f170a7
Javier Ruitti <jruitti@undec.edu.ar>
2020-09-01 12:25:12 -0300

    Agrega Test de integración (happy path) de Caso de uso entre fechas

--- MongoAdapter/src/test/java/ar/edu/undec/accidents/mongoadapter/MongoAdapterApplicationTests.java
+++ MongoAdapter/src/test/java/ar/edu/undec/accidents/mongoadapter/MongoAdapterApplicationTests.java
@@ -1,37 +1,31 @@
 package ar.edu.undec.accidents.mongoadapter;
 
-import repository.IAccidentsBetweenDatesRepository;
-import usecase.AccidentsBetweenDatesUseCase;
+import ar.edu.undec.accidents.mongoadapter.controller.dtomodel.AccidentDTO;
+import ar.edu.undec.accidents.mongoadapter.controller.service.AccidentsBetweenDatesController;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @SpringBootTest
 class MongoAdapterApplicationTests {
 
     @Autowired
-    IAccidentsBetweenDatesRepository iAccidentsBetweenDatesRepository;
+    AccidentsBetweenDatesController accidentsBetweenDatesController;
 
 
     @Test
-    void queryAccidents(){
-        AccidentsBetweenDatesUseCase accidentsBetweenDatesUseCase=new AccidentsBetweenDatesUseCase(iAccidentsBetweenDatesRepository);
-        LocalDateTime fromDate=LocalDateTime.of(2016,1,1,0,0,0);
-        LocalDateTime toDate=LocalDateTime.of(2016,3,1,10,0,0);
-        Assertions.assertEquals(1047, accidentsBetweenDatesUseCase.getAccidentsBetweenDates(fromDate,toDate).size());
-
-    }
-
-    @Test
-    void queryAccidentsEndpoint(){
-        AccidentsBetweenDatesUseCase accidentsBetweenDatesUseCase=new AccidentsBetweenDatesUseCase(iAccidentsBetweenDatesRepository);
+    void getAccidentsBetweenDates_ExistsAccidents_ReturnCollectionAnd200(){
         LocalDateTime fromDate=LocalDateTime.of(2016,1,1,0,0,0);
         LocalDateTime toDate=LocalDateTime.of(2016,12,31,10,0,0);
-        Assertions.assertEquals(1047, accidentsBetweenDatesUseCase.getAccidentsBetweenDates(fromDate,toDate).size());
-
+        ResponseEntity response=accidentsBetweenDatesController.getAccidentsBetweenDates(fromDate,toDate);
+        Assertions.assertEquals(410600,((ArrayList<AccidentDTO>)response.getBody()).size());
+        Assertions.assertEquals(HttpStatus.OK,response.getStatusCode());
     }
 
 }
