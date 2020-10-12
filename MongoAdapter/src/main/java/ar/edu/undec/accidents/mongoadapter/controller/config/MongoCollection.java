30b2b56b138de724627589555e8755bc1e2a423c
Javier Ruitti <jruitti@undec.edu.ar>
2020-10-12 15:29:48 -0300

    Actualiza parametros de conexion

--- MongoAdapter/src/main/java/ar/edu/undec/accidents/mongoadapter/controller/config/MongoCollection.java
+++ MongoAdapter/src/main/java/ar/edu/undec/accidents/mongoadapter/controller/config/MongoCollection.java
@@ -1,9 +1,5 @@
 package ar.edu.undec.accidents.mongoadapter.controller.config;
 
 public class MongoCollection {
-
-    //public static final String collectionName="accidentes";
     public static final String collectionName="accidents";
-//    public static final String collectionName="reduced";
-
 }
